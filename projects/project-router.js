a7b793416a5c929546fd27f62119ac95c48a6c27
Jenn Soderborg <jsoderborg7@gmail.com>
2019-10-11 11:22:18 -0600

    mvp met, working on endpoints because it gives me giggles

--- projects/project-router.js
+++ projects/project-router.js
@@ -14,4 +14,29 @@ router.get('/', (req, res) =>{
   })
 })
 
+router.post('/', (req, res) =>{
+  const projectData = req.body;
+  Projects.add(projectData)
+  .then(project =>{
+    res.status(201).json(project)
+  })
+  .catch(err =>{
+    res.status(500).json({message: "Could not create project"})
+  })
+})
+
+router.get('/:id', (req, res) =>{
+  Projects.findProjectById(req.params.id)
+  .then(project =>{
+    if (project) {
+      res.json(project)
+    } else {
+      res.status(404).json({message: "Project with specified ID does not exist"})
+    }
+  })
+  .catch(err =>{
+    res.status(500).json({message: "Could not get projects"})
+  })
+})
+
 module.exports = router;
